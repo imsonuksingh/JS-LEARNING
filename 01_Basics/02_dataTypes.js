27188fa7ca5a9a6894b7cf447cb2cedfc4aaf08d
SONU KUMAR SINGH <sonusinghiitian@gmail.com>
2024-06-22 00:26:32 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";  // treat all js code as newer version
+
+// alert(10 + 10) // we are using nodejs, not browser
+
+let name = "sonu"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined => 
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);      // object
